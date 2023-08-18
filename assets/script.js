5735591796bad82a2ca7049db80d3cd3f1ce2a0e
silvia brundu <silviabrundu13@gmail.com>
2023-08-18 21:04:35 +0200

    feat: comment and function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
- const slides = 
+ const slides = // creation tableau dans variable slides
 
 [
 	{
@@ -19,53 +19,58 @@
 	}
 ]
 
-// les fleches droite et gauche
-
 let positionTab = 0;
-let index = 0;
-const arrowLeft = document.querySelector('.arrow_left');
+const arrowLeft = document.querySelector('.arrow_left'); //selection fleches gauche/droite
 const arrowRight = document.querySelector('.arrow_right');
 
-for (let i = 0; i < slides.length; i ++) {
-	let dot = document.createElement('div');
-	dot.classList.add('dot');
-	document.querySelector('.dots').appendChild(dot);
+// ma boucle "for"
+
+for (let i = 0; i < slides.length; i ++) {	// Pour chaque image, création dot dans ma div dots
+	let dot = document.createElement('div');	// creation de l'element div
+	dot.classList.add('dot');	//creation de la classe dot dans la variable dot
+	document.querySelector('.dots').appendChild(dot);	// creation de la classe dot dans ma div dots
 }
 
-let dotSelected = document.querySelector('.dot');
+let dotSelected = document.querySelector('.dot'); 
 dotSelected.classList.add("dot_selected");
 
- arrowLeft.addEventListener('click', function() {
+ arrowLeft.addEventListener('click', function() { //evenement au click avec addEventListener
 
-	const dots = document.querySelectorAll('.dot');
-	dots[positionTab].classList.remove('dot_selected');
-	positionTab = positionTab-1;
-	if (positionTab < 0) {
-		positionTab = slides.length - 1;
-	  }
-	changeImage(positionTab);  
-	dots[positionTab].classList.add('dot_selected');
+	const dots = document.querySelectorAll('.dot');	// variable qui contient toutes les div dot
+	dots[positionTab].classList.remove('dot_selected');	// Enlève la classe de dot_selected grace a remove
+	positionTab = positionTab-1;	//recule de 1 a chaque click sur la fleche gauche
+	infinitySlides();
+	changeImage(positionTab);	//change image en prenant en parametre la position
+	dots[positionTab].classList.add('dot_selected');  // permet de placer la class dot_selected au cotes de la classe dot en fonction de sa position
  });
 
- arrowRight.addEventListener('click', function() {
-	
-	const dots = document.querySelectorAll('.dot');
-	dots[positionTab].classList.remove('dot_selected');
-	positionTab = positionTab+1;
-	if ( positionTab >= slides.length ) {
-		positionTab = 0;
-	  }
-	changeImage(positionTab);
-	dots[positionTab].classList.add('dot_selected');
- });
+ arrowRight.addEventListener('click', function() {	
 
-// ajout des images 
+	const dots = document.querySelectorAll('.dot');	
+	dots[positionTab].classList.remove('dot_selected');	
+	positionTab = positionTab+1;	//avance de 1 a chaque click sur la fleche droite
+	infinitySlides();
+	changeImage(positionTab);	
+	dots[positionTab].classList.add('dot_selected'); 
+});
+
+// toute mes fonctions
+
+function infinitySlides() {
+	if (positionTab < 0) {	// si ma position est inferieur a zero
+		positionTab = slides.length - 1;  //ma position est egale au nombre de slides -1
+	}
+
+	else if ( positionTab >= slides.length ) {	//si ma position et superieur ou egale au nombre de slide
+		positionTab = 0;  //je rebascule sur la premiere image
+	}
+};
 
 function changeImage(index) {
-	document.querySelector('.banner-img').src = './assets/images/slideshow/' + slides[index].image;
-	document.querySelector('#banner p').innerHTML = slides[index].tagLine;
-	
-}
+	document.querySelector('.banner-img').src = './assets/images/slideshow/' + slides[index].image;  //selection de la source des images
+	document.querySelector('#banner p').innerHTML = slides[index].tagLine;  //selection de tout les paragraphes avec innerhtml 
+};
+
 
 
 
